52d6832fbf45b13b03303283cac377fc6c3ef0f2
Adeosun <Adeosun01pluto@gmail.com>
2025-06-03 07:45:25 -0700

    hover

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -8,23 +8,23 @@
 //       <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-red-200 dark:bg-red-700 dark:border-red-600">
 //         <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
 //           <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-red-50 dark:hover:bg-red-800 group">
-//             <MdRestaurantMenu className="w-5 h-5 mb-1 text-red-500 dark:text-red-400 group-hover:text-red-600 dark:group-hover:text-red-500" />
-//             <span className="text-sm text-red-500 dark:text-red-400 group-hover:text-red-600 dark:group-hover:text-red-500">Foods</span>
+//             <MdRestaurantMenu className="w-5 h-5 mb-1 text-red-500 dark:text-red-400 " />
+//             <span className="text-sm text-red-500 dark:text-red-400 ">Foods</span>
 //           </button>
 
 //           <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-red-50 dark:hover:bg-red-800 group">
-//             <MdDeliveryDining  className="w-5 h-5 mb-1 text-red-500 dark:text-red-400 group-hover:text-red-600 dark:group-hover:text-red-500" />
-//             <span className="text-sm text-red-500 dark:text-red-400 group-hover:text-red-600 dark:group-hover:text-red-500">Logistics</span>
+//             <MdDeliveryDining  className="w-5 h-5 mb-1 text-red-500 dark:text-red-400 " />
+//             <span className="text-sm text-red-500 dark:text-red-400 ">Logistics</span>
 //           </button>
 
 //           <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-red-50 dark:hover:bg-red-800 group">
-//             <MdChecklist className="w-5 h-5 mb-1 text-red-500 dark:text-red-400 group-hover:text-red-600 dark:group-hover:text-red-500" />
-//             <span className="text-sm text-red-500 dark:text-red-400 group-hover:text-red-600 dark:group-hover:text-red-500">Errands</span>
+//             <MdChecklist className="w-5 h-5 mb-1 text-red-500 dark:text-red-400 " />
+//             <span className="text-sm text-red-500 dark:text-red-400 ">Errands</span>
 //           </button>
 
 //           <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-red-50 dark:hover:bg-red-800 group">
-//             <MdFeedback className="w-5 h-5 mb-1 text-red-500 dark:text-red-400 group-hover:text-red-600 dark:group-hover:text-red-500" />
-//             <span className="text-sm text-red-500 dark:text-red-400 group-hover:text-red-600 dark:group-hover:text-red-500">Feedback</span>
+//             <MdFeedback className="w-5 h-5 mb-1 text-red-500 dark:text-red-400 " />
+//             <span className="text-sm text-red-500 dark:text-red-400 ">Feedback</span>
 //           </button>
 //         </div>
 //       </div>
@@ -98,16 +98,16 @@ const Bottombar = () => {
                 <IconComponent 
                   className={`w-5 h-5 mb-1 ${
                     active 
-                      ? 'text-red-600 dark:text-red-500' 
-                      : 'text-red-500 dark:text-red-400'
-                  } group-hover:text-red-600 dark:group-hover:text-red-500`} 
+                      ? 'text-white' 
+                      : 'text-gray-100'
+                  } `} 
                 />
                 <span 
                   className={`text-sm ${
                     active 
-                      ? 'text-red-600 dark:text-red-500' 
-                      : 'text-red-500 dark:text-red-400'
-                  } group-hover:text-red-600 dark:group-hover:text-red-500`}
+                      ? 'text-white' 
+                      : 'text-gray-50'
+                  } `}
                 >
                   {item.label}
                 </span>
